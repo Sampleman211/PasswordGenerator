ebdaeb26d0db433f0d2424956811240284aeaede
Sampkeman211 <sampleman211@gmail.com>
2023-05-27 04:29:51 +0300

    comments

--- main.py
+++ main.py
@@ -12,31 +12,31 @@ class App(CTk.CTk):
         self.resizable(False, False)
 
 
-        self.logo = CTk.CTkImage(dark_image=Image.open("img.png"),size=(460,150))
+        self.logo = CTk.CTkImage(dark_image=Image.open("img.png"), size=(460, 150))  # Создание логотипа
         self.logo_label = CTk.CTkLabel(master=self, text="", image=self.logo)
         self.logo_label.grid(row=0, column=0)
 
-        self.password_frame = CTk.CTkFrame(master=self, fg_color="transparent")
+        self.password_frame = CTk.CTkFrame(master=self, fg_color="transparent")   # Создание фрейма для поля пароля
         self.password_frame.grid(row=1, column=0, padx=(20, 20), sticky="nsew")
 
-        self.entry_password = CTk.CTkEntry(master=self.password_frame,width=300)
-        self.entry_password.grid(row=0,column=0, padx=(0, 20))
+        self.entry_password = CTk.CTkEntry(master=self.password_frame, width=300)   # Создание поля ввода пароля
+        self.entry_password.grid(row=0, column=0, padx=(0, 20))
 
         self.btn_generate = CTk.CTkButton(master=self.password_frame, text="Generate", width=100,
-                                          command=self. set_password)
+                                          command=self. set_password)   # Создание кнопки "Generate"
         self.btn_generate.grid(row=0,column=1)
 
-        self.settings_frame = CTk.CTkFrame(master=self)
+        self.settings_frame = CTk.CTkFrame(master=self)   # Создание фрейма для настроек
         self.settings_frame.grid(row=2, column=0, padx=(20, 20), pady=(20, 0), sticky="nsew")
 
         self.password_length_slider = CTk.CTkSlider(master=self.settings_frame, from_=0, to=100, number_of_steps=100,
                                                     command=self.slider_event)
-        self.password_length_slider.grid(row=1, column=0, columnspan=3, pady=(20, 20), sticky="ew")
+        self.password_length_slider.grid(row=1, column=0, columnspan=3, pady=(20, 20), sticky="ew")   # Создание ползунка для выбора длины пароля
 
-        self.password_length_entry = CTk.CTkEntry(master=self.settings_frame, width=50)
+        self.password_length_entry = CTk.CTkEntry(master=self.settings_frame, width=50)    # Создание поля ввода для указания длины пароля
         self.password_length_entry.grid(row=1, column=3, pady=(20, 10), sticky="we")
 
-        self.cb_digits_var = tkinter.StringVar()
+        self.cb_digits_var = tkinter.StringVar()   # Создание флажков для выбора символов пароля
         self.cb_digits = CTk.CTkCheckBox(master=self.settings_frame, text="0-9", variable=self.cb_digits_var,
                                          onvalue=digits, offvalue="")
         self.cb_digits.grid(row=2, column=0, padx=10)
@@ -59,9 +59,9 @@ class App(CTk.CTk):
         self.appearance_mode_option_menu = CTk.CTkOptionMenu(master=self.settings_frame,
                                                              values=["light", "Dark" , "System"],
                                                              command=self.change_appearance_mode_event)
-        self.appearance_mode_option_menu.grid(row=3, column=0, columnspan=4, pady=(10,10))
+        self.appearance_mode_option_menu.grid(row=3, column=0, columnspan=4, pady=(10,10))   # Создание выпадающего меню для выбора режима отображения
 
-        self.password_length_slider.set(17)
+        self.password_length_slider.set(17)   # Установка начальных значений элементов интерфейса
         self.password_length_entry.insert(0, 17)
         self.appearance_mode_option_menu.set("System")
 
